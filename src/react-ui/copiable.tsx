181c8d7cd5c44bb430ffe1b81fddd0f38b5bd225
Fabio Mauri <cripty2001@outlook.com>
2025-12-03 02:34:53 +0100

    .

---
+++ src/react-ui/copiable.tsx
@@ -0,0 +1,31 @@
+import { copyToClipboard } from "@cripty2001/utils";
+import { CheckCircle2, CopyIcon } from "lucide-react";
+import { useCallback, useState } from "react";
+
+export type CopiableProps = {
+    value: string;
+    className?: string;
+}
+
+export default function Copiable({ value, className }: CopiableProps) {
+    const [copying, setCopying] = useState(false);
+
+    const copy = useCallback(() => {
+        copyToClipboard(value);
+        setCopying(true);
+        setTimeout(() => {
+            setCopying(false);
+        }, 1000);
+    }, [value, setCopying]);
+
+    return (
+        <div onClick={copy} className={`flex flex-row items-center justify-between gap-2 cursor-pointer ${className}`}>
+            <span>
+                {value}
+            </span>
+            {copying ?
+                <CheckCircle2 className="w-4 h-4" /> :
+                <CopyIcon className="w-4 h-4" />}
+        </div>
+    );
+}
--- src/react-ui/index.tsx
+++ src/react-ui/index.tsx
@@ -1,4 +1,5 @@
 import BaseButton, { type ButtonProps as BaseButtonProps } from './button';
+import BaseCopiable, { type CopiableProps as BaseCopiableProps } from './copiable';
 import BaseForm, { type FormComponentProps as BaseFormProps } from './form';
 import BaseInput, { type InputComponentProps as BaseInputProps } from './input';
 import BaseLoader, { type LoaderProps as BaseLoaderProps } from './loader';
@@ -6,8 +7,7 @@ import BaseModal, { type ModalComponentProps as BaseModalProps } from './modal';
 import BaseTable, { type TableProps as BaseTableProps } from './table';
 
 export {
-    BaseButton, BaseButtonProps,
-    BaseForm, BaseFormProps, BaseInput,
+    BaseButton, BaseButtonProps, BaseCopiable, BaseCopiableProps, BaseForm, BaseFormProps, BaseInput,
     BaseInputProps, BaseLoader,
     BaseLoaderProps, BaseModal, BaseModalProps, BaseTable, BaseTableProps
 };
