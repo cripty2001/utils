2e46484fd2a25c8312d6848c92a4bf6d2fc80c14
Fabio Mauri <cripty2001@outlook.com>
2025-12-24 01:19:23 +0100

    .

--- src/react-ui/button.tsx
+++ src/react-ui/button.tsx
@@ -1,14 +1,14 @@
 import { Loader2, LucideIcon } from "lucide-react";
 import { useState } from "react";
 
+
 export type ButtonProps = {
-    title: string,
-    icon?: React.ReactElement<LucideIcon>,
     onClick: () => void | Promise<void>
+    children: React.ReactElement
     className?: string
 }
 
-export default function Button({ title, onClick, className, icon }: ButtonProps) {
+export function Button({ onClick, children, className }: ButtonProps) {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState<string | null>(null)
     const handleClick = async () => {
@@ -43,15 +43,28 @@ export default function Button({ title, onClick, className, icon }: ButtonProps)
                         height: '1rem',
                         animation: 'spin 1s linear infinite',
                     }} /> :
-                    <div className="flex flex-row gap-4 items-center justify-center">
-                        {icon}
-                        {title}
-                    </div>
+                    children
                 }
             </div>
             {error && <div style={{
                 color: '#ef4444'
             }}>{error}</div>}
         </>
+    )
+}
+
+export type TextButtonProps = Omit<ButtonProps, 'children'> & {
+    title: string,
+    icon?: React.ReactElement<LucideIcon>
+}
+
+export function TextButton({ title, onClick, className, icon }: TextButtonProps) {
+    return (
+        <Button onClick={onClick} className={className}>
+            <div className="flex flex-row gap-4 items-center justify-center">
+                {icon}
+                {title}
+            </div>
+        </Button>
     )
 }
